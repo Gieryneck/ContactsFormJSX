9a557a0092b9954d0304167f54d32fea98ed98af
Gieryn <gierynt@gmail.com>
2017-11-26 23:58:14 +0100

    wtf

--- components/Contact.js
+++ components/Contact.js
@@ -11,9 +11,9 @@ var Contact = React.createClass({
         return (
     
             <div className={'contactItem'}>
-                <img className={contactImage} src={'http://icons.veryicon.com/ico/System/100%20Flat%20Vol.%202/contacts.ico'}/>>
-                <p className={'contactLabel'}>Imię: {this.props.item.firstName}</p>
-                <p className={'contactLabel'}>Nazwisko: {this.props.item.lastName}</p>
+                <img className={'contactImage'} src={'http://icons.veryicon.com/ico/System/100%20Flat%20Vol.%202/contacts.ico'}/>>
+                <p className={'contactLabel'}> Imię: {this.props.item.firstName}</p>
+                <p className={'contactLabel'}> Nazwisko: {this.props.item.lastName}</p>
                 <a className={'contactLabel'} href={'mailto:' + this.props.item.email}>
                     {this.props.item.email}
                 </a>
